e2aa8b9fda013361d46bfb4a8ad574c0532fdc54
GahBaek <127074448+GahBaek@users.noreply.github.com>
2024-06-15 17:16:19 +0900

    feature: spring-init - #1

---
+++ src/test/java/com/example/airplaneletter/AirplaneLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airplaneletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneLetterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
