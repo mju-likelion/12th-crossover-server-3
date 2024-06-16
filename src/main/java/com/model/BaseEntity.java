fc531bbc59db87b58019b111854e1320ff602766
GahBaek <127074448+GahBaek@users.noreply.github.com>
2024-06-16 14:04:12 +0900

    feature: add PostController - #5

--- src/main/java/com/model/BaseEntity.java
+++ src/main/java/com/model/BaseEntity.java
@@ -1,4 +1,4 @@
-package model;
+package com.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
--- src/main/java/com/model/TermUser.java
+++ src/main/java/com/model/TermUser.java
@@ -1,4 +1,4 @@
-package model;
+package com.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
